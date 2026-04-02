a65c23a0bad73b17dbd12fc978cc6779d0f9f33c
Progtrix <360veljko@gmail.com>
2026-04-03 01:01:50 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    #if __cplusplus == 199711L
+        std::cout << "C++98/03\n";
+    #elif __cplusplus == 201103L
+        std::cout << "C++11\n";
+    #elif __cplusplus == 201402L
+        std::cout << "C++14\n";
+    #elif __cplusplus == 201703L
+        std::cout << "C++17\n";
+    #elif __cplusplus == 202002L
+        std::cout << "C++20\n";
+    #elif __cplusplus > 202002L
+        std::cout << "C++23 ili noviji\n";
+    #else
+        std::cout << "Nepoznat standard\n";
+    #endif
+
+
+    std::cout << "Hello World";
+
+}
